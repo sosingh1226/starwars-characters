0a5d8a79a392be4dcea67fe417cd4e9330744b29
soumyas <so.singh1226@gmail.com>
2020-12-16 18:12:36 -0500

    entered PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const apiRoutes = require("./routes/api-routes")
 const clientRoutes = require("./routes/client-routes");
